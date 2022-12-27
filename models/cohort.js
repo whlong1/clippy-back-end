00cb1e7c2bf4062882440299d3e90a9c9e9f6e0a
whlong1 <whunterlong@gmail.com>
2022-12-27 12:47:59 -0500

    Build cohortSchema

---
+++ models/cohort.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const cohortSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  },
+  instructors: [{ type: mongoose.Types.ObjectId, ref: 'Profile' }],
+  ias: [{ type: mongoose.Types.ObjectId, ref: 'Profile' }],
+  tas: [{ type: mongoose.Types.ObjectId, ref: 'Profile' }],
+  students: [{ type: mongoose.Types.ObjectId, ref: 'Profile' }],
+  waitlist: [{ type: mongoose.Types.ObjectId, ref: 'Profile' }],
+  inactive: [{ type: mongoose.Types.ObjectId, ref: 'Profile' }],
+  attendance: [{ type: mongoose.Types.ObjectId, ref: 'Attendance' }],
+  deliverables: [{ type: mongoose.Types.ObjectId, ref: 'Deliverable' }],
+}, {
+  timestamps: true,
+})
+
+const Cohort = mongoose.model('Cohort', cohortSchema)
+
+export { Cohort }
