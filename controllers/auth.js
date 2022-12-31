e52f30c0ed1d864f78bebebbf6f59aaaac288af9
whlong1 <whunterlong@gmail.com>
2022-12-31 12:08:37 -0500

    Clean up getUserDataFromToken controller function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,11 @@
 import { Profile } from '../models/profile/profile.js'
-// import { getAccessToken, getRoles } from '../helpers/auth0.js'
 
 async function getUserDataFromToken(req, res) {
   try {
-    // console.log('REQ.AUTH', req.auth)
+    // The 'user_id' value we need can be found on req.auth.sub:
     const profile = await Profile.findOne({ user_id: req.auth.sub })
     if (profile) {
-      // console.log('Profile', profile)
-      // Getting user roles is redundant after front end fix.
-      // Should we leave this in as an added security measure?
-
-      // const accessToken = await getAccessToken()
-      // const roles = await getRoles(accessToken, req.auth.sub)
-      // profile.admin = roles.some((r) => r.name === 'Admin')
-      // console.log(roles)
-      
-
-      res.json(profile)
+      res.status(200).json(profile)
     } else {
       throw Error('Profile not found!')
     }
